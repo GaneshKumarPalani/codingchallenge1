b8cde96d66ca48bcb26e5b2a05231ceb863d5e6f
Ganesh Kumar Palani <ganeshkumar.p@listertechnologies.com>
2020-07-21 22:39:41 +0530

    Permutations of a list of numbers

---
+++ Permutations/permutations.js
@@ -0,0 +1,23 @@
+function permutation(inputArray) {
+    
+    let result = [];
+  
+    for (let i = 0; i < inputArray.length; i = i + 1) {
+      let permutationArray = permutation(inputArray.slice(0, i).concat(inputArray.slice(i + 1)));
+  
+      if(!permutationArray.length) {
+        result.push([inputArray[i]])
+      } else {
+        for(let j = 0; j < permutationArray.length; j = j + 1) {
+            result.push([inputArray[i]].concat(permutationArray[j]))
+        }
+      }
+    }
+
+    return result;
+  
+}
+
+exports.runPremutation = (inputArray) => {
+    return permutation(inputArray);
+};
