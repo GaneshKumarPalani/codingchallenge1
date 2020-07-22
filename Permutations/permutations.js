6efa0fb31a014f4243b19a0b024d97990b501fd6
Ganesh Kumar Palani <ganeshkumar.p@listertechnologies.com>
2020-07-22 13:08:33 +0530

    Updated the format to ES6 standard

--- Permutations/permutations.js
+++ Permutations/permutations.js
@@ -1,11 +1,11 @@
 const permutation = (inputArray) => {
     let result = [];
-    for (let i = 0; i < inputArray.length; i = i + 1) {
+    for (let i = 0; i < inputArray.length; i++) {
       let permutationArray = permutation(inputArray.slice(0, i).concat(inputArray.slice(i + 1)));
       if(!permutationArray.length) {
         result.push([inputArray[i]])
       } else {
-        for(let j = 0; j < permutationArray.length; j = j + 1) {
+        for(let j = 0; j < permutationArray.length; j++) {
             result.push([inputArray[i]].concat(permutationArray[j]))
         }
       }
